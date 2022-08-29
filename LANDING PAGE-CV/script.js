0c05b702b269c89b9166cb8b5a882c870cb5c146
Johan David <johan.dpl@gmail.com>
2022-08-29 11:51:37 -0500

    Add files via upload

---
+++ LANDING PAGE-CV/script.js
@@ -0,0 +1,55 @@
+// MENU
+
+((d) => {
+  const btnMenu = d.querySelector(".menu-btn");
+  const menu = d.querySelector(".menu");
+
+  btnMenu.addEventListener("click", () => {
+    btnMenu.firstElementChild.classList.toggle("none");
+    btnMenu.lastElementChild.classList.toggle("none");
+    menu.classList.toggle("is-visible");
+  });
+
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+
+    btnMenu.firstElementChild.classList.toggle("none");
+    btnMenu.lastElementChild.classList.toggle("none");
+    menu.classList.remove("is-visible");
+  });
+})(document);
+
+((d)=>{
+  const $form = d.querySelector(".contact-form");
+  const $loader = d.querySelector(".contact-form-loader");
+  const $response = d.querySelector("contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/johan.dpl_16@hotmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
+})(document);
